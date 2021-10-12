864ad37b3b75b2613cdfb0a21a04ac546a38eead
horizonzy <1060026287@qq.com>
2021-10-13 02:16:25 +0800

    complete.

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/kitex-contrib/registry-zookeeper
 
 go 1.16
+
+require (
+	github.com/cloudwego/kitex v0.0.1
+	github.com/go-zookeeper/zk v1.0.2
+	github.com/stretchr/testify v1.7.0
+)
---
+++ utils/net.go
@@ -0,0 +1,47 @@
+// Copyright 2021 CloudWeGo authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific
+
+package utils
+
+import "net"
+
+func ParseIP(s string) (net.IP, int) {
+	ip := net.ParseIP(s)
+	if ip == nil {
+		return nil, 0
+	}
+	for i := 0; i < len(s); i++ {
+		switch s[i] {
+		case '.':
+			return ip, 4
+		case ':':
+			return ip, 6
+		}
+	}
+	return nil, 0
+}
+
+func GetLocalIPv4Address() (ipv4Address string, err error) {
+	addrs, err := net.InterfaceAddrs()
+
+	for _, addr := range addrs {
+		ipNet, isIpNet := addr.(*net.IPNet)
+		if isIpNet && !ipNet.IP.IsLoopback() {
+			ipv4 := ipNet.IP.To4()
+			if ipv4 != nil {
+				return ipv4.String(), nil
+			}
+		}
+	}
+	return
+}
